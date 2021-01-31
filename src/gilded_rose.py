dde9192def4ac4c28b634e31170d2f1d65d490cb
VictorPorlan <vporlan@cifpfbmoll.eu>
2021-01-31 13:55:26 +0100

    feat: :zap: nuevas clases creadas

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -17,5 +17,55 @@ class Item:
 
 
 class Updateable:
-    def upadte_quality(self):
+    def update_quality(self):
         pass
+
+class NormalItem(Item, Updateable):
+    def __init__(self, name, sell_in, quality):
+        Item.__init__(self, name, sell_in, quality)
+
+    def setSell_in(self):
+        self.sell_in -= 1
+
+    def setQuality(self, valor):
+        if self.quality + valor > 50:
+            self.quality = 50
+        elif self.quality + valor >= 0:
+            self.quality = self.quality + valor
+        else:
+            self.quality = 0
+            
+    def update_quality(self):
+        if self.sell_in > 0:
+            self.Quality(-1)
+        else:
+            self.Quality(-2)
+        self.setSell_in()           
+
+class ConjuredItem(NormalItem):
+    def __init__(self, name, sell_in, quality):
+        Item.__init__(self, name, sell_in, quality)
+    
+    def setSell_in(self):
+        NormalItem.setSell_in()
+
+    def setQuality(self, valor):
+        NormalItem.setQuality(self,valor)
+
+    def update_quality(self):
+        if self.sell_in > 0:
+            self.setQuality(-2)
+        else:
+            self.setQuality(-4)
+        self.setSell_in()
+
+class AgedBrie (Item, Updateable):
+    def __init__(self, name, sell_in, quality):
+        NormalItem.__init__(self, name, sell_in, quality)
+
+    def update_quality(self):
+        if self.sell_in > 0:
+            self.setQuality(1)
+        else:
+            self.setQuality(1)
+        self.setSell_in()
